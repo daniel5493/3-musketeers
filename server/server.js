fd304618b1373193cdf7a254852f33cdcd4a7669
daniel5493 <daniel5493@hotmail.com>
2024-05-08 23:45:56 -0500

    update

--- server/server.js
+++ server/server.js
@@ -34,12 +34,12 @@ server.applyMiddleware({ app, path: '/graphql' });
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
   } 
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 
 // Start the server
 app.listen(PORT, () => {
