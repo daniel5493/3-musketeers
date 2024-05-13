b611351c90fcc5939ca935c3c193725a602a9b7c
daniel5493 <daniel5493@hotmail.com>
2024-05-13 07:39:51 -0500

    rebuilt and added --force to install commands

--- server/server.js
+++ server/server.js
@@ -28,12 +28,12 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-    
-    app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
   } 
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
