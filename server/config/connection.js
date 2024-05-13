b93036861911f6848e5706228a61e3aec2e6ece9
daniel5493 <daniel5493@hotmail.com>
2024-05-13 08:52:04 -0500

    last try

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/budgettracker');
+mongoose.connect('mongodb+srv://daniel5493:Koolaid5493@cluster0.wtjyffr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' || 'mongodb://127.0.0.1:27017/budgettracker');
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -26,9 +26,6 @@ const startApolloServer = async () => {
     context: authMiddleware
   }));
 
-  // if we're in production, serve client/dist as static assets
-  if (process.env.NODE_ENV === 'production') {
-  } 
   app.use(express.static(path.join(__dirname, '../client/dist')));
   
   app.get('/', (req, res) => {
